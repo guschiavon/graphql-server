631d481acbac94cb3128a739e40b1fe23dbe139c
Gus <gustavo.schiavon@gmail.com>
2020-08-14 12:35:16 +0800

    add graphql queries for application use

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,14 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :email, String, null: true
+    field :posts, [Types::PostType], null: true
+    field :post_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    
+    def post_count
+      object.posts.count
+    end
   end
+
 end
